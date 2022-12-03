401780fb55889e64de4c2388bb3b88260e139660
Timur <Stafr95@mail.ru>
2022-12-03 21:54:30 +0500

    add file and block reviews

--- src/js/script.js
+++ src/js/script.js
@@ -122,6 +122,26 @@ function setClock(selector, endtime) {
 setClock('.timer', deadline);
 
 
+const swiper = new Swiper('.swiper-container', {
+	grabCursor: true,
+	centeredSlides: true,
+	freeMode: true,
+	slidesPerView: 'auto',
+	effect: 'coverflow',
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows : true
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true
+	}
+});
+
+
 
 
 
