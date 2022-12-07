0362cb929ec9a8a007c2967e2b552397c129a8c4
Timur <Stafr95@mail.ru>
2022-12-07 22:42:58 +0500

    add form

--- src/js/script.js
+++ src/js/script.js
@@ -105,6 +105,44 @@ const swiper = new Swiper('.swiper-container', {
 	}
 });
 
+			// Modal
+
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+	modal = document.querySelector('.popup'),
+	modalCloseBtn = document.querySelector('[data-close]');
+
+
+modalTrigger.forEach(btn => {
+	btn.addEventListener('click', () => {
+		modal.classList.add('show');
+		modal.classList.remove('hide');
+		document.body.style.overflow = 'hidden';
+	})
+})
+
+function closeModal() {
+	modal.classList.add('hide');
+	modal.classList.remove('show');
+	document.body.style.overflow = '';
+}
+
+modalCloseBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+	if (e.target === modal) {
+		closeModal();
+	}
+});
+
+document.addEventListener('keydown', (e) => {
+	if (e.code === 'Escape' && modal.classList.contains('show')) {
+		closeModal();
+	}
+});
+
+
+
+
 
 
 
