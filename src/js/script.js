569d1318cd771f680f5541ff2209cc175c6dbaa2
Timur <Stafr95@mail.ru>
2022-12-19 22:16:03 +0500

    update burger

--- src/js/script.js
+++ src/js/script.js
@@ -85,6 +85,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	setClock('.timer', deadline);
 
+	// Burger
+
+	const openMobileMenuBtn = document.querySelector(".device-menu");
+	const headerMenu = document.querySelector(".header-nav");
+
+	openMobileMenuBtn.addEventListener("click", () => {
+		if(openMobileMenuBtn.classList.contains("open")) {
+			openMobileMenuBtn.classList.remove("open");
+			headerMenu.classList.remove("active");
+		} else {
+			headerMenu.classList.add("active");
+			openMobileMenuBtn.classList.add("open");
+		}
+	})
+
+
 	// Swiper
 
 	const swiper = new Swiper('.swiper-container', {
